4246f487c62e9b9463a28b8b6ff7100e4916d942
Abe White <abewhite@mac.com>
2023-01-09 15:37:54 -0600

    Generate some test code

--- Plugins/JXBridgeGeneratorTool/BridgeGeneratorTool.swift
+++ Plugins/JXBridgeGeneratorTool/BridgeGeneratorTool.swift
@@ -4,11 +4,12 @@ import PackagePlugin
 /// Built plugin to invoke our `BridgeGenerator` tool with source files.
 @main struct BridgeGeneratorTool: BuildToolPlugin {
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        let name = target.name
+        let outputFile = context.pluginWorkDirectory.appending(subpath: "JXGenerated.swift")
         let inputDir = target.directory
-        let outputDir = context.pluginWorkDirectory
         let bridgeGenerator = try context.tool(named: "JXBridgeGenerator").path
         return [
-            .buildCommand(displayName: "JXBridgeBuilder", executable: bridgeGenerator, arguments: [inputDir, outputDir])
+            .buildCommand(displayName: "JXBridgeBuilder", executable: bridgeGenerator, arguments: [name, outputFile, inputDir], outputFiles: [outputFile])
         ]
     }
 }
@@ -16,12 +17,16 @@ import PackagePlugin
 #if canImport(XcodeProjectPlugin)
 import XcodeProjectPlugin
 
-//~~~
 extension BridgeGeneratorTool: XcodeBuildToolPlugin {
     func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        let name = target.product?.name ?? ""
+        let outputFile = context.pluginWorkDirectory.appending(subpath: "JXGenerated.swift")
         let bridgeGenerator = try context.tool(named: "JXBridgeGenerator").path
+        let arguments = target.inputFiles
+            .filter { $0.type == .source }
+            .map { $0.path.string }
         return [
-            .buildCommand(displayName: "JXBridgeBuilder", executable: bridgeGenerator, arguments: ["Argument1", "Argument2"])
+            .buildCommand(displayName: "JXBridgeBuilder", executable: bridgeGenerator, arguments: [name, outputFile] + arguments, outputFiles: [outputFile])
         ]
     }
 }
--- Sources/JXBridgeGenerator/BridgeGenerator.swift
+++ Sources/JXBridgeGenerator/BridgeGenerator.swift
@@ -1,70 +1,84 @@
 import Foundation
 #if os(macOS) || os(Linux)
-import SourceKittenFramework
+//import SourceKittenFramework
 
 @main
 public class BridgeGenerator {
     static func main() throws {
         let arguments = CommandLine.arguments
-        guard arguments.count == 3 else {
-            throw BridgeGeneratorError.usage()
+        for argument in arguments {
+            print("ARGUMENT: \(argument)")
         }
 
-        setenv("IN_PROCESS_SOURCEKIT", "YES", 1) // Don't do this when running in Xcode?
-        let inputDirectory = URL(fileURLWithPath: arguments[1], isDirectory: true)
-        let outputDirectory = URL(fileURLWithPath: arguments[2], isDirectory: true)
-        try BridgeGenerator(inputDirectory: inputDirectory, outputDirectory: outputDirectory).run()
-    }
-
-    let inputDirectory: URL
-    let outputDirectory: URL
-
-    init(inputDirectory: URL, outputDirectory: URL) {
-        self.inputDirectory = inputDirectory
-        self.outputDirectory = outputDirectory
-    }
-
-    func run() throws {
-        guard let directoryEnumerator = FileManager.default.enumerator(at: inputDirectory, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles]) else {
+        guard arguments.count >= 3 else {
+            print("TOO FEW ARGUMENTS!")
             return
+            //throw BridgeGeneratorError.usage()
         }
-        for case let fileURL as URL in directoryEnumerator {
-            guard fileURL.pathExtension == "swift", let resourceValues = try? fileURL.resourceValues(forKeys: [.isRegularFileKey]),
-                  resourceValues.isRegularFile == true else {
-                continue
-            }
-            guard let file = File(path: fileURL.path) else {
-                continue
-            }
-            print("FILE: \(fileURL)") //~~~
-            let structure = try Structure(file: file)
-            //print("STRUCTURE: \(structure.description)")
-            //print("========================================")
-            processCandidate(dictionary: structure.dictionary)
-
-        }
-    }
-
-    private func processCandidate(dictionary: [String: SourceKitRepresentable]) {
-        if let inheritedTypes = dictionary["key.inheritedtypes"] as? [[String: SourceKitRepresentable]], let name = dictionary["key.name"] as? String {
-            if inheritedTypes.contains(where: { $0["key.name"] as? String == "JXStaticBridging" }) {
-                print("FOUND NAME: \(name)")
-            }
-        }
-        if let substructures = dictionary["key.substructure"] as? [[String: SourceKitRepresentable]] {
-            for substructure in substructures {
-                processCandidate(dictionary: substructure)
-            }
-        }
+        let name = arguments[1]
+        let outputFile = arguments[2]
+        try """
+public class JXGeneratedClass {
+    public var name: String {
+        return "\(name)"
     }
 }
+""".write(toFile: outputFile, atomically: true, encoding: .utf8)
+
+// OR...
+
+//        setenv("IN_PROCESS_SOURCEKIT", "YES", 1) // Don't do this when running in Xcode?
+//        let inputDirectory = URL(fileURLWithPath: arguments[1], isDirectory: true)
+//        let outputDirectory = URL(fileURLWithPath: arguments[2], isDirectory: true)
+//        try BridgeGenerator(inputDirectory: inputDirectory, outputDirectory: outputDirectory).run()
+    }
+
+//    let inputDirectory: URL
+//
+//    init(inputDirectory: URL) {
+//        self.inputDirectory = inputDirectory
+//    }
+//
+//    func run() throws {
+//        guard let directoryEnumerator = FileManager.default.enumerator(at: inputDirectory, includingPropertiesForKeys: [.isRegularFileKey], options: [.skipsHiddenFiles]) else {
+//            return
+//        }
+//        for case let fileURL as URL in directoryEnumerator {
+//            guard fileURL.pathExtension == "swift", let resourceValues = try? fileURL.resourceValues(forKeys: [.isRegularFileKey]),
+//                  resourceValues.isRegularFile == true else {
+//                continue
+//            }
+//            guard let file = File(path: fileURL.path) else {
+//                continue
+//            }
+//            print("FILE: \(fileURL)") //~~~
+//            let structure = try Structure(file: file)
+//            //print("STRUCTURE: \(structure.description)")
+//            //print("========================================")
+//            processCandidate(dictionary: structure.dictionary)
+//
+//        }
+//    }
+//
+//    private func processCandidate(dictionary: [String: SourceKitRepresentable]) {
+//        if let inheritedTypes = dictionary["key.inheritedtypes"] as? [[String: SourceKitRepresentable]], let name = dictionary["key.name"] as? String {
+//            if inheritedTypes.contains(where: { $0["key.name"] as? String == "JXStaticBridging" }) {
+//                print("FOUND NAME: \(name)")
+//            }
+//        }
+//        if let substructures = dictionary["key.substructure"] as? [[String: SourceKitRepresentable]] {
+//            for substructure in substructures {
+//                processCandidate(dictionary: substructure)
+//            }
+//        }
+//    }
+}
 #else
-//~~~
 @main
 public class BridgeGenerator {
     static func main() throws {
         let argumentsString = CommandLine.arguments.joined(separator: ",")
-        throw BridgeGeneratorError(message: "SHIT BAD RUN ON \(argumentsString)")
+        throw BridgeGeneratorError(message: "Unsupported platform")
     }
 }
 #endif
